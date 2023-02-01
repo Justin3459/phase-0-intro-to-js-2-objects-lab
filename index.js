59a51b8e6145acbdf6d44ff94fb2f410d4c637e0
justin becker <justin3459@hotmail.com>
2023-02-01 13:35:28 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {
+    name: "Justin",
+    streetAddress: "123 road",
+}
+ function updateEmployeeWithKeyAndValue(employee, name, Sam){
+   const updateEmployee = {...employee}
+   updateEmployee[name] = Sam
+   return updateEmployee
+ }
+ function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, ){
+    employee[streetAddress] = "12 Broadway"
+    return employee
+  }
+ function deleteFromEmployeeByKey(employee, name){
+   const deleteEmployee = {...employee}
+    delete deleteEmployee.name
+    return deleteEmployee
+ }
+ function destructivelyDeleteFromEmployeeByKey(employee, name){
+    delete employee.name
+    return employee
+ }
